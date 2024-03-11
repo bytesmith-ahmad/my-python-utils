7afa5d4c1eda959299da23e639ed36194244687f
The Codesmith <b-9k@outlook.com>
2024-03-10 22:17:31 -0400

    quick commit

---
+++ type_library.py
@@ -0,0 +1,7 @@
+from typing import TypeVar, Self
+
+ClassObject = TypeVar("ClassObject")
+SectionProxy = TypeVar("SectionProxy")
+Report = TypeVar("Report")
+Command = TypeVar("Command")
+ConnectCommand = TypeVar("ConnectCommand")
