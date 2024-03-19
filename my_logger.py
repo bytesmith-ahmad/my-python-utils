998f58cdad104d7fdd69e59a2a7e775c16ac4cc6
The Codesmith <b-9k@outlook.com>
2024-03-19 14:47:27 -0400

    updated my logger, added more robust filepath handling and try-except

--- my_logger.py
+++ my_logger.py
@@ -1,18 +1,31 @@
 # Setup logger to my own tastes for debugging purposes
-
+import os
 import configparser
 import logging
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+try:
+    target = f"{__file__}{'\\..\\..\\config.ini'}"
+    fallback = f"C:\\Users\\{os.getlogin()}\\Desktop\\otolith.log"
 
-if not logging.getLogger().hasHandlers():
-    logging.basicConfig(filename=config['Logfile']['filename'],
-                        filemode='w',
-                        level=logging.INFO,
-                        format='%(asctime)s [%(filename)s:%(lineno)d] %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S'
-                        )
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    logging.getLogger('').addHandler(console)
+    config = configparser.ConfigParser()
+    config.read(target)
+
+    if not logging.getLogger().hasHandlers():
+        logging.basicConfig(filename=config['Logfile'].get('filepath',fallback),
+                            filemode='w',
+                            level=logging.INFO,
+                            format='%(asctime)s [%(filename)s:%(lineno)d] %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S'
+                            )
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+        logging.getLogger('').addHandler(console)
+        
+        def info(msg:str):
+            logging.info(msg)
+            
+        def exception(msg:str):
+            logging.exception(msg)
+                              
+except Exception as e:
+    raise ImportError("Error importing my_logger")
