721b4d7c8997cf760292c0d11ccbf271a663461a
The Codesmith <b-9k@outlook.com>
2024-03-28 21:52:21 -0400

    .

--- src/type_library.py
+++ src/type_library.py
@@ -2,6 +2,5 @@ from typing import TypeVar, Self
 
 ClassObject = TypeVar("ClassObject")
 SectionProxy = TypeVar("SectionProxy")
-Report = TypeVar("Report")
 Command = TypeVar("Command")
 ConnectCommand = TypeVar("ConnectCommand")
